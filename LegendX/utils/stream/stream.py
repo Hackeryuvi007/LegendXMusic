697a9610d6aaf8573850cd217d749f742f46a9e6
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-09-18 10:16:06 +0530

    🖤

--- LegendX/utils/stream/stream.py
+++ LegendX/utils/stream/stream.py
@@ -110,8 +110,8 @@ async def stream(
                     original_chat_id,
                     photo=img,
                     caption=_["stream_1"].format(
-                        title[:30], duration_min, user_name
-                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        title, duration_min, user_name
+                        
                     ),
                     reply_markup=InlineKeyboardMarkup(button),
                 )
@@ -191,8 +191,8 @@ async def stream(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
-                    title[:30], duration_min, user_name
-                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    title, duration_min, user_name
+                    
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
